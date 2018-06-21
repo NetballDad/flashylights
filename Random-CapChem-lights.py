7d7a2979367896f2b2136632806aab6c4c6bd344
NetballDad <netballdad.au@hammo.id.au>
2018-06-21 18:03:26 +1000

    short form random

--- Random-CapChem-lights.py
+++ Random-CapChem-lights.py
@@ -242,19 +242,18 @@ strip.clear()
 
 lights = random.randint(0, 5)
 
-switch (lights) {
-        case 1: splitPixels(getColour("blue"));
-            break;
-        case 2: ticktock(3, 5, getColour("blue"), getColour("white"))
-            break;
-        case 3: chasePixels(120, -5, -10, getColour("white"), getColour("blue"))
-            break;
-        case 4: chaseRandomPixels(5, 1, getColour("White"), getColour("blue"))
-            break;
-        case 5: setAllPixels("blue")
+
+        if lights == 1: splitPixels(getColour("blue"))
+            pass
+        elseif lights == 2: ticktock(3, 5, getColour("blue"), getColour("white"))
+            pass
+        elseif lights == 3: chasePixels(120, -5, -10, getColour("white"), getColour("blue"))
+            pass
+        elseif lights == 4:chaseRandomPixels(5, 1, getColour("White"), getColour("blue"))
+            pass
+        elseif lights == 5: setAllPixels("blue")
                 flashLights(0.5)
-                break;
-        case 6: setAllPixels("white")
+                pass
+        elseif lights == 6: setAllPixels("white")
                 flashLights(0.5)
-                break;
-}
+                pass
