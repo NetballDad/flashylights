57b1fadf33f7be0022ca9b1afc76100a19b5ccd3
RandomDrew <31381520+RandomDrew@users.noreply.github.com>
2018-08-11 09:18:01 +1000

    initial add

--- Transfer_to_S3.py
+++ Transfer_to_S3.py
@@ -6,7 +6,9 @@ bucket = s3.Bucket('netball-ml-processing')
 
 print(bucket.objects)
 
-s3.meta.client.upload_file('/Users/andrewhammond/s3_upload.jpg','netball-ml-processing', 's3_upload.jpg')
+#s3.meta.client.upload_file('/Users/andrewhammond/s3_upload.jpg','netball-ml-processing', 's3_upload.jpg')
+
+s3.meta.client.upload_file('../99-20180621221554-05.jpg','netball-ml-processing', '99-20180621221554-05.jpg')
 
 #so simple
 
