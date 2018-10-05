5dbf573fc493a0f9bffd09509b58c6559eaaf451
NetballDad <netballdad.au@hammo.id.au>
2018-10-05 23:19:51 +1000

    debugging is bugging

--- label-image-aws.py
+++ label-image-aws.py
@@ -31,7 +31,7 @@ s3 = boto3.resource('s3')
 bucket = s3.Bucket('netball-ml-processing')
 
 #now connect to the DynamoDB
-dynamoDB = boto3resource('dynamodb')
+dynamoDB = boto3.resource('dynamodb')
 dTable = dynamoDB.table('netball-ml-results')
 
 
